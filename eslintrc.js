7a751e43a06ef717d7fa8d20aba7dbaeb00f73d5
michal <grafogeum@users.noreply.github.com>
2022-06-11 16:25:53 +0200

    initial projects list feat: sorting filtering

---
+++ eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ['prettier', 'react-app', 'react-app/jest'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error'],
+  },
+};
--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import './App.scss';
+import { Button } from '@material-ui/core';
 
-function App() {
+const ProjectRow = ({ project }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <tr key={project.id}>
+      <td>{project.id}</td>
+      <td>{project.name}</td>
+      <td>{project.country}</td>
+      <td>{project.industry}</td>
+      <td>{project.numberOfEmployees}</td>
+    </tr>
   );
-}
+};
 
+const App = () => {
+  const [projects, projectsSet] = React.useState([]);
+  const [filter, filterSet] = React.useState('');
+  const [order, orderSet] = React.useState('asc');
+
+  const sorting = (col) => {
+    if (order === 'asc') {
+      const sorted = [...projects].sort((a, b) => (a[col] > b[col] ? 1 : -1));
+      projectsSet(sorted);
+      orderSet('dsc');
+    }
+    if (order === 'dsc') {
+      const sorted = [...projects].sort((a, b) => (a[col] < b[col] ? 1 : -1));
+      projectsSet(sorted);
+      orderSet('asc');
+    }
+  };
+
+  React.useEffect(() => {
+    fetch('http://localhost:3000/projects.json')
+      .then((res) => res.json())
+      .then((data) => {
+        projectsSet(data);
+      });
+  }, []);
+
+  return (
+    <>
+      <div
+        style={{
+          margin: 'auto',
+          width: 800,
+          paddingTop: '2rem',
+          fontSize: 'large',
+        }}
+      >
+        <h1>Projects</h1>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => {
+            filterSet('China');
+          }}
+        >
+          {' '}
+          China
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => {
+            filterSet('United States');
+          }}
+        >
+          {' '}
+          USA
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => {
+            filterSet('');
+          }}
+        >
+          {' '}
+          All
+        </Button>
+        <input
+          type="text"
+          placeholder="Filter"
+          value={filter}
+          onChange={(e) => filterSet(e.target.value)}
+        />
+        <table width="100%">
+          <thead>
+            <tr>
+              <th onClick={() => sorting('id')}>Id</th>
+              <th onClick={() => sorting('name')}>{order === 'asc' ? '⬆️' : '⬇️'}Name</th>
+              <th>Country</th>
+              <th>Industry</th>
+              <th onClick={() => sorting('numberOfEmployees')}>
+                {order === 'asc' ? '⬆️' : '⬇️'}Employees
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {projects
+              .filter((project) => project.country.toLowerCase().includes(filter.toLowerCase()))
+              .map((project) => (
+                <ProjectRow key={project.id} project={project} />
+              ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
 export default App;
